e47f6f14f11006690f9e44698e2bbcc2c21e4ce9
MAYANK CHHIPA <40556224+mgroovyank@users.noreply.github.com>
2020-06-02 16:27:04 +0530

    Update task_builder.py

--- finetune/task_builder.py
+++ finetune/task_builder.py
@@ -28,14 +28,14 @@ import os
 import tensorflow.compat.v1 as tf
 
 from finetune import feature_spec
-from finetune import task
 from util import utils
 
 import numpy as np
 import scipy
 import sklearn
 
-
+from typing import List, Tuple
+from model import modeling
 
 class Scorer(object):
   """Abstract base class for computing evaluation metrics."""
@@ -105,8 +105,55 @@ class AccuracyScorer(SentenceLevelScorer):
 
 
 
+class Example(object):
+  __metaclass__ = abc.ABCMeta
 
-class InputExample(task.Example):
+  def __init__(self, task_name):
+    self.task_name = task_name
+
+
+class Task(object):
+  """Override this class to add a new fine-tuning task."""
+
+  __metaclass__ = abc.ABCMeta
+
+  def __init__(self, config: configure_finetuning.FinetuningConfig, name):
+    self.config = config
+    self.name = name
+
+  def get_test_splits(self):
+    return ["test"]
+
+  @abc.abstractmethod
+  def get_examples(self, split):
+    pass
+
+  @abc.abstractmethod
+  def get_scorer(self) -> scorer.Scorer:
+    pass
+
+  @abc.abstractmethod
+  def get_feature_specs(self) -> List[feature_spec.FeatureSpec]:
+    pass
+
+  @abc.abstractmethod
+  def featurize(self, example: Example, is_training: bool,
+                log: bool=False):
+    pass
+
+  @abc.abstractmethod
+  def get_prediction_module(
+      self, bert_model: modeling.BertModel, features: dict, is_training: bool,
+      percent_done: float) -> Tuple:
+    pass
+
+  def __repr__(self):
+    return "Task(" + self.name + ")"
+
+
+
+
+class InputExample(Example):
   """A single training/test example for simple sequence classification."""
 
   def __init__(self, eid, task_name, text_a, text_b=None, label=None):
@@ -117,7 +164,7 @@ class InputExample(task.Example):
     self.label = label
 
 
-class SingleOutputTask(task.Task):
+class SingleOutputTask(Task):
   """Task with a single prediction per example (e.g., text classification)."""
 
   __metaclass__ = abc.ABCMeta
