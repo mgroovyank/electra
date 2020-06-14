dfda8f302718002282268841419a7a0f2f7fa80e
MAYANK CHHIPA <40556224+mgroovyank@users.noreply.github.com>
2020-06-14 11:57:39 +0530

    Update run_finetuning.py

--- run_finetuning.py
+++ run_finetuning.py
@@ -104,20 +104,6 @@ class FinetuningConfig(object):
     self.double_unordered = True  # for tasks like paraphrase where sentence
                                   # order doesn't matter, train the model on
                                   # on both sentence orderings for each example
-    # for qa tasks
-    self.max_query_length = 64   # max tokens in q as opposed to context
-    self.doc_stride = 128  # stride when splitting doc into multiple examples
-    self.n_best_size = 20  # number of predictions per example to save
-    self.max_answer_length = 30  # filter out answers longer than this length
-    self.answerable_classifier = True  # answerable classifier for SQuAD 2.0
-    self.answerable_uses_start_logits = True  # more advanced answerable
-                                              # classifier using predicted start
-    self.answerable_weight = 0.5  # weight for answerability loss
-    self.joint_prediction = True  # jointly predict the start and end positions
-                                  # of the answer span
-    self.beam_size = 20  # beam size when doing joint predictions
-    self.qa_na_threshold = -2.75  # threshold for "no answer" when writing SQuAD
-                                  # 2.0 test outputs
 
     # TPU settings
     self.use_tpu = False
